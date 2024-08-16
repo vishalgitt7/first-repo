ed75b22bc8a3186fed8914a3fb8403fe4c474474
vishalgitt7 <joevishal333@gmail.com>
2024-08-16 14:33:54 +0530

    removed terraform-files extra

--- project-2048/terraform-files/security-group/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "vpc_id" {
-  description = "The VPC ID where the security group will be created"
-}
-
-variable "eks_security_group_id" {
-  description = "The security group ID of the EKS cluster"
-}
