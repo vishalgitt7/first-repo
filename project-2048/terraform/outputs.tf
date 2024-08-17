938f662a29cdb90fad29d08fcfc140705f878597
vishalgitt7 <joevishal333@gmail.com>
2024-08-17 18:39:50 +0530

    removed terraform-files extra

--- project-2048/terraform/outputs.tf
+++ project-2048/terraform/outputs.tf
@@ -1,16 +1,23 @@
-output "cluster_name" {
-  value = module.eks.cluster_name
+output "cluster_id" {
+  description = "EKS cluster ID."
+  value       = module.eks.cluster_id
 }
 
 output "cluster_endpoint" {
-  value = module.eks.cluster_endpoint
+  description = "Endpoint for EKS control plane."
+  value       = module.eks.cluster_endpoint
 }
 
-output "cluster_certificate_authority_data" {
-  value = module.eks.cluster_certificate_authority_data
+output "cluster_security_group_id" {
+  description = "Security group ids attached to the cluster control plane."
+  value       = module.eks.cluster_security_group_id
 }
 
-output "security_group_id" {
-  value = module.securitygroup.security_group_id
+output "region" {
+  description = "AWS region"
+  value       = var.aws_region
 }
 
+output "oidc_provider_arn" {
+  value = module.eks.oidc_provider_arn
+}
--- project-2048/terraform/variables.tf
+++ project-2048/terraform/variables.tf
@@ -1,5 +1,13 @@
-variable "aws_region" {
-  description = "The AWS region to deploy to"
-  default     = "us-east-1"
+variable "kubernetes_version" {
+  default     = 1.27
+  description = "kubernetes version"
 }
 
+variable "vpc_cidr" {
+  default     = "10.0.0.0/16"
+  description = "default CIDR range of the VPC"
+}
+variable "aws_region" {
+  default = "us-east-1"
+  description = "aws region"
+}
