12ed7bd51658f554291f39c1493d99a3ba85aeb4
vishalgitt7 <joevishal333@gmail.com>
2024-08-17 11:14:12 +0530

    Update main.tf

--- project-2048/terraform/eks/main.tf
+++ project-2048/terraform/eks/main.tf
@@ -3,9 +3,9 @@ module "eks" {
   cluster_name    = "my-eks-cluster"
   cluster_version = "1.21"
   vpc_id           = var.vpc_id
-  subnets          = var.public_subnets  # Use private_subnets if required by your setup
+  subnets_id          = var.public_subnets  # Use private_subnets if required by your setup
 
-  node_groups = {
+  node_group = {
     eks_nodes = {
       desired_capacity = 2
       max_capacity     = 3
