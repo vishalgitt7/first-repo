b0e316fd1ea931164bbe9c1e93bd1947886feb0b
vishalgitt7 <joevishal333@gmail.com>
2024-08-18 22:58:59 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,6 +2,6 @@
 # /root/2048-scripts/scripts/start_application.sh
 
 # Apply the Kubernetes manifests
-kubectl apply -f scripts/deployment.yaml
-kubectl apply -f scripts/service.yaml
-
+#kubectl apply -f scripts/deployment.yaml
+#kubectl apply -f scripts/service.yaml
+echo "start the application by copying yaml files"
