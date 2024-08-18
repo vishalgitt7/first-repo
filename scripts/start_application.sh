c51353fafcc3b9d2e4c849d18e8bd23f4d424807
vishalgitt7 <joevishal333@gmail.com>
2024-08-18 22:56:47 +0530

    Update start_application.sh

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -2,6 +2,6 @@
 # /root/2048-scripts/scripts/start_application.sh
 
 # Apply the Kubernetes manifests
-kubectl apply -f /root/2048-scripts/scripts/deployment.yaml
-kubectl apply -f /root/2048-scripts/scripts/service.yaml
+kubectl apply -f scripts/deployment.yaml
+kubectl apply -f scripts/service.yaml
 
