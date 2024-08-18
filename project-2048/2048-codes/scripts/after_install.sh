52d03fe4f6aa21ec4e8081b8fc033252b12b6289
vishalgitt7 <joevishal333@gmail.com>
2024-08-18 20:27:29 +0530

    Update after_install.sh

--- project-2048/2048-codes/scripts/after_install.sh
+++ project-2048/2048-codes/scripts/after_install.sh
@@ -2,4 +2,4 @@
 # This script runs after the installation process
 
 # Configure access to the Kubernetes cluster
-aws eks --region us-east-1 update-kubeconfig --name <your-cluster-name>
+aws eks --region us-east-1 update-kubeconfig --name 2048-eks-WbeQJfwY
