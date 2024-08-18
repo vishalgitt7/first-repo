ce62481d9b4eafd8f09f1fdbe59f17de3122c493
vishalgitt7 <joevishal333@gmail.com>
2024-08-18 22:48:05 +0530

    Update after_install.sh

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 # This script runs after the installation process
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo yum install unzip -y
+sudo ./aws/install
 
 # Configure access to the Kubernetes cluster
 aws eks --region us-east-1 update-kubeconfig --name 2048-eks-WbeQJfwY
