bf0328f1b0ca27df6fcce46b6ce45759d516b2b1
7vishalroy <joevishal333@gmail.com>
2025-02-05 21:02:29 +0530

    Update app.py

--- project-2048/2048-codes/app.py
+++ project-2048/2048-codes/app.py
@@ -7,17 +7,5 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    import os
-    from waitress import serve  # Waitress for production
-    
-    # Get environment mode (development or production)
-    env = os.getenv("FLASK_ENV", "development")
-    
-    if env == "production":
-        print("Running with Waitress WSGI server...")
-        serve(app, host="0.0.0.0", port=8000)
-    else:
-        print("Running in development mode...")
-        app.run(host="0.0.0.0", port=5000, debug=True)
-
+    app.run(host="0.0.0.0", port=5000)
 
