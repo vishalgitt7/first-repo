1fea1f06d68f2fdc31f306232ed1486762d9ecc4
7vishalroy <joevishal333@gmail.com>
2025-02-05 21:40:13 +0530

    changed code app.py

--- project-2048/2048-codes/app.py
+++ project-2048/2048-codes/app.py
@@ -8,17 +8,14 @@ def home():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    print(f"Current environment: {os.getenv('FLASK_ENV', 'development')}")
+    print(f"Current environment: {os.getenv('FLASK_ENV', 'production')}")
     
     from waitress import serve  # Waitress for production
     
-    env = os.getenv("FLASK_ENV", "development")
+    env = os.getenv("FLASK_ENV", "production")
     
-    if env == "production":
-        print("Running with Waitress WSGI server...")
-        serve(app, host="0.0.0.0", port=8000)
-    else:
-        print("Running in development mode...")
-        app.run(host="0.0.0.0", port=5000, debug=True)
+    print("Running with Waitress WSGI server...")
+    serve(app, host="0.0.0.0", port=8000)
+
 
 
